73a48488c4dc9a708232fcf8ce50409d499e3b1b
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-08-28 21:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;
+    
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+});
